2c929cbfc0f19580393571295d62fe77bbbfc91e
Sergei Volchkov <c4s23@yandex.ru>
2026-03-11 18:05:07 +0300

    feat: add aact.config.ts to ecommerce-structurizr CLI example

---
+++ examples/ecommerce-structurizr/aact.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "../../src";
+
+export default defineConfig({
+  source: {
+    type: "structurizr",
+    path: "./workspace.json",
+    writePath: "./workspace.dsl",
+  },
+
+  rules: {
+    acl: true,
+    acyclic: true,
+    crud: true,
+    dbPerService: true,
+    cohesion: true,
+    stableDependencies: true,
+  },
+});
